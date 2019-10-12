a3aecfad2990221a7a1029ac56483a87e70f13f2
StevanCakic <stevancakic93@gmail.com>
2019-10-12 17:57:16 +0200

    content for class 1 attached

---
+++ Week 1/code-5/examples.js
@@ -0,0 +1,10 @@
+/* Napisati funkciju koja uklanja sve falsy vrijednosti iz niza
+Koristiti splice ili napraviti novi niz koji sadrži truthy vrijednosti iz starog niza, 
+koristi arr.push(elem) za dodavanje novog elementa u niz */
+
+/* Napisati program koji iz niza brojeva od 1 do 20 sumira sve parne brojeve, a za neparne nalazi proizvod */
+
+/* Napisati funkciju koja vraća najmanji indeks na čijoj poziciji treba da se 
+doda vrijednost (drugi argument) u niz (prvi argument) koji treba da ostane sortiran
+fun([1,2,3,4], 1.5) -> 1
+fun([20,2,3], 19) ->2 jer kad se niz sortira (2,3,20) element 19 može da se doda između 3 i 20, tj. na indeksu 2 */
